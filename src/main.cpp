c04938584e9770b028c6e7a4346d92d14168b67e
jlt20 <72909581+jlt20@users.noreply.github.com>
2023-02-22 11:55:03 +0000

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,25 @@
 
 //Constants
   const uint32_t interval = 100; //Display update interval
+  const double A = 440; 
+  const double twelfth_root_2 = 1.05946309436;
+  volatile int32_t currentStepSize;
+  const int32_t stepSizes[] = {
+    (int32_t) (4294967296.0 * A / (pow(twelfth_root_2, 9)) * 22000.0), // C
+    (int32_t) (4294967296.0 * A / (pow(twelfth_root_2, 8)) * 22000.0), // C#
+    (int32_t) (4294967296.0 * A / (pow(twelfth_root_2, 7)) * 22000.0), // D
+    (int32_t) (4294967296.0 * A / (pow(twelfth_root_2, 6)) * 22000.0), // D#
+    (int32_t) (4294967296.0 * A / (pow(twelfth_root_2, 5)) * 22000.0), // E
+    (int32_t) (4294967296.0 * A / (pow(twelfth_root_2, 4)) * 22000.0), // F
+    (int32_t) (4294967296.0 * A / (pow(twelfth_root_2, 3)) * 22000.0),  // F#
+    (int32_t) (4294967296.0 * A / (pow(twelfth_root_2, 2)) * 22000.0),  // G
+    (int32_t) (4294967296.0 * A / (pow(twelfth_root_2, 1)) * 22000.0),  // G#
+    (int32_t) (4294967296.0 * A / 22000.0),  // A
+    (int32_t) (4294967296.0 * A * pow(twelfth_root_2, 1) / 22000.0),  // A#
+    (int32_t) (4294967296.0 * A * pow(twelfth_root_2, 2) / 22000.0),  // B
+};
+
+  uint8_t keyArray[7];
 
 //Pin definitions
   //Row select and enable
@@ -19,7 +38,7 @@
   const int OUT_PIN = D11;
 
   //Audio analogue out
-  const int OUTL_PIN = A4;
+  const int OUTL_PIN = A;
   const int OUTR_PIN = A3;
 
   //Joystick analogue in
@@ -79,23 +98,62 @@ void setup() {
   Serial.println("Hello World");
 }
 
+uint8_t readCols(){
+  uint8_t result = 0;   
+  result |= digitalRead(C0_PIN) << 0;
+  result |= digitalRead(C1_PIN) << 1;
+  result |= digitalRead(C2_PIN) << 2;
+  result |= digitalRead(C3_PIN) << 3;
+
+  return result;
+}
+
+void setRow(uint8_t rowIdx){
+  digitalWrite(REN_PIN, LOW); // Disable row select enable
+  digitalWrite(RA0_PIN, rowIdx & 0x01);
+  digitalWrite(RA1_PIN, rowIdx & 0x02);
+  digitalWrite(RA2_PIN, rowIdx & 0x04);
+  digitalWrite(REN_PIN, HIGH); // Enable row select enable
+}
+
+void scanSwitchMatrix(){
+  for(uint8_t row; row<3; row++){
+    setRow(row);
+
+    delayMicroseconds(100); 
+
+    uint8_t cols = readCols();
+    keyArray[row] = cols; 
+  }
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
   static uint32_t next = millis();
   static uint32_t count = 0;
-
+  
   if (millis() > next) {
     next += interval;
 
+    scanSwitchMatrix();
+    uint16_t hexData = (keyArray[2] << 8) | (keyArray[1] << 4) | keyArray[0];
+    Serial.print("Keys: ");
+    for (int i = 0; i < 3; i++) {
+      Serial.print(keyArray[i], HEX);
+    }
+    Serial.println();
     //Update display
     u8g2.clearBuffer();         // clear the internal memory
     u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
     u8g2.drawStr(2,10,"Helllo World!");  // write something to the internal memory
     u8g2.setCursor(2,20);
-    u8g2.print(count++);
+    uint8_t keys = readCols();
+    u8g2.setCursor(2,20);
+    u8g2.print(keys,HEX); 
     u8g2.sendBuffer();          // transfer internal memory to the display
 
     //Toggle LED
     digitalToggle(LED_BUILTIN);
   }
+
 }
